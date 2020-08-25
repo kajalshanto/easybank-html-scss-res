2e370bc78a40e096d8523eb367b69f0b4f802513
Hasibul Hasan Shanto <hasibulhasan.kajal@gmail.com>
2020-08-25 22:32:59 +0600

    complete nav adding hero

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,40 @@
+// Hamburger Menu
+const btnHamburger = document.querySelector('#hamuburger');
+const body = document.querySelector('body');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
+
+
+btnHamburger.addEventListener('click', function () {
+
+    if (header.classList.contains('open')) { // Close hamburger
+        body.classList.remove('noscroll');
+        header.classList.remove('open');
+
+        fadeElems.forEach(function (element) {
+            element.classList.add('fade-out');
+            element.classList.remove('fade-in');
+            element.classList.add('has-fade');
+
+        });
+
+    } else { // Open hamburger
+        body.classList.add('noscroll');
+        header.classList.add('open');
+
+        fadeElems.forEach(function (element) {
+            element.classList.add('fade-in');
+            element.classList.remove('fade-out');
+            element.classList.remove('has-fade');
+
+        });
+    }
+});
+
+
+
+
+
 //footer current year with this following
-document.getElementById("year").innerHTML = new Date().getFullYear();
+//document.getElementById("year").innerHTML = new Date().getFullYear();
